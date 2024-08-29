23fe88ff41529a2acc6d477cd75b5b61c0fa7e5a
itgl <itgel6708@gmail.com>
2024-08-29 21:05:28 +0800

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { ModeToggle } from "./Toggle";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+    const { theme, setTheme } = useTheme()
 
     return (
-        <div className="sticky top-0 z-50  w-full  backdrop-filter backdrop-blur-3xl">
-            <div className="px-10 flex justify-between items-center max-width:100% border-b-[1px]">
+        <div className="fixed w-full  backdrop-filter backdrop-blur-3xl">
+            <div className="flex justify-between items-center max-width:100% border-b-[1px] px-4 sm:p-4">
                 <div className="text-2xl font-bold">
-                    {/* logo image here */}
-                    {/* <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" /> */}
-                    {/* <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" /> */}
-                    <Image src="/white.png" className="absolute rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" alt="logo" width={150} height={150} />
-                    <Image src="/dark.png" className=" rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" alt="logo" width={150} height={150} />
+                    <Image
+                        // check dark mode
+                        src={theme !== "dark" ? "/image_white.png" : "/image_dark.png"}
+                        alt="logo" 
+                        width="0"
+                        height="0"
+                        sizes={"(max-width: 640px) 200vw, (max-width: 1024px) 200vw, 1024px"}
+                        style={{ width: '100%', height: 'auto' }}  
+                    />
+                    
                 </div>
-                <div className="flex space-x-4">
-                    <a href="#home" className="text-lg pt-1 dark:text-gray-500 text-balck hover:text-gray-300 dark:hover:text-white">Бидний тухай</a>
-                    <a href="#product" className="text-lg pt-1 dark:text-gray-500 text-balck hover:text-gray-300 dark:hover:text-white">Бүтээгдэхүүн</a>
-                    <a href="#price" className="text-lg pt-1 dark:text-gray-500 text-balck hover:text-gray-300 dark:hover:text-white">Үнэ</a>
-                    <a href="#contact" className="text-lg pt-1 dark:text-gray-500 text-balck hover:text-gray-300 dark:hover:text-white">Холбоо барих</a>
-                    <ModeToggle />
+                <div className="flex items-center">
+                    <div className="space-x-4">
+                        <a href="#home" className="text-sm invisible sm:visible sm:text-lg dark:text-gray-500 text-balck hover:text-gray-300 dark:hover:text-white">About us</a>
+                        <a href="#product" className="text-sm invisible sm:visible sm:text-lg dark:text-gray-500 text-balck hover:text-gray-300 dark:hover:text-white">Products</a>
+                        <a href="#contact" className="text-sm  invisible sm:visible sm:text-lg dark:text-gray-500 text-balck hover:text-gray-300 dark:hover:text-white">Contact</a>
+                    </div>
+                    <div className="py-1 px-2">
+                        <ModeToggle  />
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }
--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,17 +1,54 @@
 import React from "react";
 import Image from "next/image";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
-export default function Product({logosvg, title, desciption}: {logosvg: any,title: string, desciption: string}) {
+
+export default function Product({logosvg, title, desciption, longDescription}: {logosvg: any,title: string, desciption: string, longDescription:any}) {
    
     return (
-        <div className="p-4 px-6 flex max-width:100% border-2 rounded-xl">
-            <div className="pt-2 w-10 h-10">
-                {logosvg}
-            </div>
-            <div className="pl-5">
-                <div className="font-bold text-xl">{title}</div>
-                <p className="text-sm">{desciption}</p>
-            </div>
+        <div className="p-4 px-6 max-width:100% border-2 rounded-xl">
+            <Dialog >
+                <DialogTrigger>
+                <div className="flex items-start max-width:100%">
+                    <div className="pt-2 w-10 h-10">
+                        {logosvg}
+                    </div>
+                    <div className="pl-5">
+                        <div className="font-bold text-xl text-start">{title}</div>
+                        <p className="text-sm text-justify">{desciption}</p>
+                    </div>
+                    
+                    
+                </div>
+                </DialogTrigger>
+                
+                <DialogContent className="lg:max-w-screen-lg overflow-y-scroll max-h-screen">
+                    
+                        <DialogHeader>
+                        
+                        <div className="flex items-start">
+                            <div className="pt-2 w-10 h-10">
+                                {logosvg}
+                            </div>
+                            <div className="pl-5">
+                                <DialogTitle className="pb-2">{title}</DialogTitle>
+                                <DialogDescription>
+                                    {longDescription}
+                                </DialogDescription>
+                            </div>
+                        </div>
+                        </DialogHeader>
+                </DialogContent>
+                
+            </Dialog>
         </div>
     )
 }
